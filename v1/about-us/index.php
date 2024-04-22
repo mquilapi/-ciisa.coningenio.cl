797ba6485ced2c4f4be95eb14ba731641cd386d0
mquilapi <167723168+mquilapi@users.noreply.github.com>
2024-04-21 20:29:30 -0400

    primer commit

---
+++ v1/about-us/index.php
@@ -0,0 +1,57 @@
+<?php
+include_once '../version1.php';
+
+//parametros
+$existeId = false;
+$valorId = 0;
+
+if(count($_parametros)>0){
+    foreach($_parametros as $p){
+        if(strpos($p,'id') !== false){
+            $existeId = true;
+            $valorId = explode('=',$p)[1];
+        }
+    }
+}
+
+if($_version == 'v1'){
+    if($_mantenedor == 'about-us'){
+        switch ($_metodo){
+            case 'GET':
+                if($_header == $_token_get){
+                            /*include_once 'controller.php';
+                             include_once '../conexion.php';
+                             $control = new Controlador();
+                             $lista = $control -> getAll();
+                             http_response_code(200);
+                            echo json_encode(['data' => $lista]); */
+                    
+                    $retorno = [
+                        [
+                            "id" => 0,
+                            "nombre" => 'martin',
+                            "activo" => true
+                        ],
+                        [
+                            "id" => 1,
+                            "nombre" => 'Pepe',
+                            "activo" => false
+                        ],
+                        [
+                            "id" => 2,
+                            "nombre" => 'Jose',
+                            "activo" => true
+                        ],
+                    ];
+                    http_response_code(200);
+                    echo json_encode(['data' => $retorno]);
+                }else{
+                    http_response_code(401);
+                    echo json_encode(['error' => 'no tiene autorizacion get']);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ v1/index.php
@@ -0,0 +1,3 @@
+<?php
+echo 'hola';
+var_dump($_SERVER);
